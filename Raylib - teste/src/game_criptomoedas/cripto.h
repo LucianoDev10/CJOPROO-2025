e8aff093b73700d29a00bc2ed400933bbc987316
LucianoDev10 <97753074+LucianoDev10@users.noreply.github.com>
2025-06-10 01:04:47 -0300

    Delete Raylib - teste directory

--- Raylib - teste/src/game_criptomoedas/cripto.h
+++
@@ -1,7 +0,0 @@
-#ifndef CRIPTO_RUSH_H
-#define CRIPTO_RUSH_H
-
-
-void RunCriptoRush(int &currentScreen);
-
-#endif
--- Raylib - teste/src/game_vida_universitaria/vida_universitaria.h
+++
@@ -1,7 +0,0 @@
-#ifndef VIDA_UNIVERSITARIA_H
-#define VIDA_UNIVERSITARIA_H
-
-
-void RunVidaUniversitaria(int &currentScreen);
-
-#endif
